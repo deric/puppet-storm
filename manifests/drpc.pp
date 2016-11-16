06484e1118bb612bfcb924fdeb1bbfd541e88172
Joshua Knarr <Joshua.Knarr@LexisNexis.com>
2016-11-16 17:41:18 -0500

    wherein I decide to param the rest of the templates too

--- manifests/drpc.pp
+++ manifests/drpc.pp
@@ -9,21 +9,33 @@
 #  class{'storm::drpc': }
 #
 class storm::drpc(
+  $authorizer_acl_filename         = 'drpc-auth-acl.yaml',
+  $authorizer_acl_strict           = false,
+  $childopts                       = '-Xmx768m',
   $manage_service                  = false,
   $enable                          = true,
   $ensure_service                  = 'running',
   $force_provider                  = undef,
+  $max_buffer_size                 = 1048576,
   $mem                             = '1024m',
   $jvm                             = [
     '-Dlog4j.configuration=file:/etc/storm/storm.log.properties',
     '-Dlogfile.name=drpc.log'],
   $port                            = 3772,
+  $queue_size                      = 128,
   $servers                         = [''],
+  $http_port                       = 3774,
+  $https_port                      = '-1',
+  $invocations_threads             = 64,
   $invocations_port                = 3773,
+  $https_keystore_password         = '',
+  $https_keystore_type             = 'JKS',
+  $http_creds_plugin               = 'backtype.storm.security.auth.DefaultHttpCredentialsPlugin',
   $request_timeout_secs            = 600,
   $transactional_zookeeper_root    = '/transactional',
   $transactional_zookeeper_servers = 'null',
   $transactional_zookeeper_port    = 'null',
+  $worker_threads                  = 64,
   $config_file                     = $storm::config_file
 ) inherits storm {
 
